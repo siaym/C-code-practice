8337a9ce5765f0f339477742cd6d76f2d4c715be
siaym <saifulsiam10000@gmail.com>
2026-02-18 00:48:46 +0600

    Refactor code structure for improved readability and maintainability

--- 7 Data structer/tesst1.c
+++ 7 Data structer/tesst1.c
@@ -6,34 +6,51 @@ struct node
     struct node *next;
 };
 
-void deleteatfirst(int data){
+struct node *head=NULL;
 
+void insertatlast(int data){
+
+  struct node *newnode =(struct node*)  malloc(sizeof(struct node));
+  newnode->data=data;
+  newnode->next=NULL;
+
+  if (head==NULL)
+  {
+    head=newnode;
+    return;
+  }
+  struct node *temp= head;
+  while (temp->next!=NULL)
+  {
+    temp=temp->next;
+  }
+
+  temp->next=newnode;
+  
+}
+void deletefirst(){
+    struct node *k=temp;
+}
+
+void print(){
+    struct node *temp=head;
+    while (temp!=NULL)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    
 }
 
 int main (){
 
     int n=10;
-
-
-}
-
-
-
-void insertlast(int data)
+for (int   i = 1; i <= n; i++)
 {
-    struct node *newnode = malloc(sizeof(struct node));
-    newnode->data = data;
-    newnode->next = NULL;
-
-    if(head == NULL)
-    {
-        head = newnode;
-        return;
-    }
-
-    struct node *temp = head;
-    while(temp->next != NULL)
-        temp = temp->next;
-
-    temp->next = newnode;
+    insertatlast(i);
 }
+print();
+
+
+}
+
