3c0ccb1e62c9fea5347f61c7f00e224cc4776c36
siaym <saifulsiam10000@gmail.com>
2026-02-19 23:15:01 +0600

    Add a doubly linked list implementation for student records
    - Created a new file `practice1.c` to implement a doubly linked list.
    - Defined a `node` structure to hold student information (roll number, CGPA, name).
    - Implemented functions to create a new node and insert nodes at the end of the list.
    - Added a function to print the list in reverse order.
    - Included a main function to demonstrate the insertion and printing of student records.

---
+++ 7 Data structer/practice1.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node
+{
+    int roll;
+    float cgpa;
+    char name[50];
+
+    struct node *next,*prev;
+};
+
+struct node *head=NULL;
+
+struct node *createnode(int roll , float cgpa , char name[] )
+{
+    struct node *newnode=malloc(sizeof(struct node));
+    newnode->roll=roll;
+    newnode->cgpa=cgpa;
+    strcpy(newnode->name,name);
+    newnode->prev=NULL;
+    newnode->next=NULL;
+    return newnode;
+};
+
+
+void insertlast(int roll,float cgpa,char name[] ){
+    struct node *newnode=createnode(roll,cgpa,name);
+
+    if (head==NULL)
+    {
+        head=newnode;
+        return;
+    }
+    struct node *temp=head;
+    while (temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+
+    temp->next=newnode;
+    newnode->prev=temp;
+
+}
+
+void printback(){
+    struct node  *temp=head;
+    while (temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    while (temp!=NULL)
+    {
+        printf("%d- %.2f- %s",temp->roll,temp->cgpa,temp->name);
+        temp=temp->prev;
+    }
+    
+}
+
+int main (){
+
+insertlast(10,3.6,"raiyan");
+    printback();
+
+
+
+}
